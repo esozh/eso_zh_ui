c00f0bef403565481443f73c4b956d99effdb0e0
bssthu <bssthu@users.noreply.github.com>
2018-02-04 15:14:49 +0800

    编写一些简易的脚本

---
+++ 工具/更新翻译/2_合并新旧翻译.bat
@@ -0,0 +1,9 @@
+@echo off
+
+echo ###正在合并
+cd ../../scripts/
+python merge_xls_dir.py ../输出/更新翻译/1_new/ ../输出/更新翻译/4_old/ ../输出/更新翻译/2_diff/
+
+echo ###正在设置格式
+python apply_xls_format.py ../输出/更新翻译/1_new/ ../输出/更新翻译/2_diff/
+pause
