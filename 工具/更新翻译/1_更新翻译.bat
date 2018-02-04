5c7c14dd9c48961a8dc40c77baf78f2284cd59b3
bssthu <bssthu@users.noreply.github.com>
2018-02-04 18:29:36 +0800

    fix bug

--- 工具/更新翻译/1_更新翻译.bat
+++ 工具/更新翻译/1_更新翻译.bat
@@ -3,9 +3,6 @@
 REM 老滚安装路径
 set ESO_PATH=F:\Program Files (x86)\SteamLibrary\steamapps\common\Zenimax Online\The Elder Scrolls Online
 
-REM 要合并的翻译文件，文件名不要有空格
-set TRANSLATE_FILE=ESO_套装名_V0.3.xlsx
-
 
 set /p datestr="请输入文件后缀: (例如: 20180204)"
 echo %datestr%
@@ -19,23 +16,23 @@ del /Q ..\..\输出\更新翻译\4_old\*
 set MNF_PATH="%ESO_PATH%\depot\eso.mnf"
 echo %MNF_PATH%
 echo ###正在解压...
-rem del /Q .\extract\*
-rem EsoExtractData.exe %MNF_PATH% -a 0 .\extract
-rem EsoExtractData.exe %MNF_PATH% -a 2 .\extract
+del /Q ..\..\temp\extract\*
+EsoExtractData.exe %MNF_PATH% -a 0 ..\..\temp\extract
+EsoExtractData.exe %MNF_PATH% -a 2 ..\..\temp\extract
 
 echo ###正在复制...
-copy extract\gamedata\lang\en.lang.csv ..\..\translation\lang\
-copy extract\gamedata\lang\jp.lang.csv ..\..\translation\lang\
-copy extract\esoui\lang\en_pregame.lua ..\..\translation\
-copy extract\esoui\lang\en_client.lua ..\..\translation\
+copy ..\..\temp\extract\gamedata\lang\en.lang.csv ..\..\translation\lang\
+copy ..\..\temp\extract\gamedata\lang\jp.lang.csv ..\..\translation\lang\
+copy ..\..\temp\extract\esoui\lang\en_pregame.lua ..\..\translation\
+copy ..\..\temp\extract\esoui\lang\en_client.lua ..\..\translation\
 
 echo ###正在拆分...
 cd ../../scripts
-rem python split_lang_csv_by_id.py
-rem python split_lang_csv_by_id.py -l jp
+python split_lang_csv_by_id.py
+python split_lang_csv_by_id.py -l jp
 
 echo ###正在提取...
-rem python prepare_lang.py --all
+python prepare_lang.py --all
 copy ..\translation\lang\en.*s.lang.xlsx ..\输出\更新翻译\1_new\
 
 del /Q ..\translation\zh_translate.txt
