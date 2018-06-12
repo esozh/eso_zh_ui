db0414eb2813a645849003206857e7d1a2b1ca5a
bssthu <bssthu@users.noreply.github.com>
2018-06-12 23:15:13 +0800

    不要检查\n的数量是否一致了

--- scripts/utils/check_xls.py
+++ scripts/utils/check_xls.py
@@ -154,7 +154,15 @@ def count_string_attr(text_to_check):
     Args:
         text_to_check (str): 一个待检查的字符串
     """
-    count = {'<>': 0, 'c': 0, 't': 0, 'bs': 0, 'n': 0, 'q': 0, '$d': 0}
+    count = {
+        '<>': 0,
+        'c': 0,
+        't': 0,
+        'bs': 0,
+        # 'n': 0,
+        'q': 0,
+        '$d': 0
+    }
     len_text = len(text_to_check)
 
     for i in range(0, len_text - 1):
@@ -168,8 +176,8 @@ def count_string_attr(text_to_check):
             count['t'] += 1
         elif text_to_check[i] == '\\' and text_to_check[i+1] == '\\':
             count['bs'] += 1
-        elif text_to_check[i] == '\\' and text_to_check[i+1] == 'n':
-            count['n'] += 1
+        # elif text_to_check[i] == '\\' and text_to_check[i+1] == 'n':
+        #     count['n'] += 1
         elif text_to_check[i] == '\\' and text_to_check[i+1] == '"':
             count['q'] += 1
         elif text_to_check[i] == '$' and text_to_check[i+1] == 'd':
